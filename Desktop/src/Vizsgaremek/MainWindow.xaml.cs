5d0208809c3cc9f1bb29ae10c056723d41de5fa5
sz0grikri <sz0grikri@vasvari.org>
2021-12-02 12:12:13 +0100

    Navigate kész

--- Desktop/src/Vizsgaremek/MainWindow.xaml.cs
+++ Desktop/src/Vizsgaremek/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vizsgaremek.Navigation;
+using Vizsgaremek.Pages;
 
 namespace Vizsgaremek
 {
@@ -23,6 +25,10 @@ namespace Vizsgaremek
         public MainWindow()
         {
             InitializeComponent();
+            Navigate.mainWindow = this;
+            WelcomePage welcomePage = new WelcomePage();
+            //Megjelenítsük a welcomepaget
+            Navigate.Navigation(welcomePage);
         }
     }
 }
