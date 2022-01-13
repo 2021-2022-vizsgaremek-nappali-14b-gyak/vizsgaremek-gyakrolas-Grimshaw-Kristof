c39df487627819ea8f1b666c21b8671141331dbe
sz0grikri <sz0grikri@vasvari.org>
2022-01-13 08:31:04 +0100

    Database source osztály a modelbe

---
+++ Desktop/src/Vizsgaremek/ViewModels/DatabaseSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizsgaremek.ViewModels
+{
+        public enum DbSource { NONE, LOCALHOST, DEVOPS }
+
+        public class DatabaseSource
+        {
+            DbSource dbSource;
+
+            private DbSource DbSource { get => dbSource; set => dbSource = value; }
+
+            public DatabaseSource()
+            {
+                this.DbSource = DbSource.NONE;
+            }
+
+        }
+    }
